5b8de9da8d13e26a06de2c382edb140c4438648e
流萤 <43709531+timeworn@users.noreply.github.com>
2025-01-01 17:28:35 -0800

    Update Anilist.ts

--- src/Anilist/Anilist.ts
+++ src/Anilist/Anilist.ts
@@ -428,14 +428,16 @@ export class Anilist implements SearchResultsProviding, MangaProgressProviding {
                     }
                     mutation = saveMangaProgressMutation(mutationData)
                 }
-
                 console.log(JSON.stringify(mutation, null, 2)) // Log request data
 
-                await this.requestManager.schedule(App.createRequest({
+                const response = await this.requestManager.schedule(App.createRequest({
                     url: ANILIST_GRAPHQL_ENDPOINT,
                     method: 'POST',
                     data: mutation
                 }), 1)
+
+                setTimeout(() => { throw new Error(JSON.stringify(response.data)) }, 0)
+                setTimeout(() => { throw new Error(JSON.stringify(mutationData)) }, 0)
             }
         })
     }
